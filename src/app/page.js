1c3c509902c51e6982f66655e5d4aa32239c8472
kabeer-necrox <khushkabeer@gmail.com>
2024-02-02 12:42:37 +0300

    this is the props

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,28 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="components">
+      <User name="kabeer"/>
+      {/* <User name="furqan"/> */}
+      <User name="developer" age= "19" address="nyc"/>
+      <User name="desiner"/>
+      <User name="hacker"/>
+      </div>
+      
       <h1 className="pagehome">Home Page</h1>
+      <p className="homepagepara">this is para</p>
       
     </main>
   );
+}
+
+
+const User =(props)=>{
+  return(
+    <>
+    <h1>user name is {props.name}</h1>
+    <p>this is { props.address} </p>
+    <p>this is {props.age} </p>
+    </>
+  )
 }
