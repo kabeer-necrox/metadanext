e2877d3e03496904d26d4e846a0e7b106a040b3b
kabeer-necrox <khushkabeer@gmail.com>
2024-02-09 23:11:51 +0300

    image optimazation

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <h1>this is the common layout of myweb </h1>
+      <h1 style={{color:"pink", border:"5px"}}>this is the common layout of myweb </h1>
       <body className={inter.className}>{children}</body>
       
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,27 @@
-{
-  ("use client");
-}
-import productlist from './productlist/page'
-import Link from "next/link";
+
+// import productlist from './productlist/page'
+// import Link from "next/link";
+// {'use client'}
+
+import Image from "next/image";
+import profile from '../../public/vercel.svg'
+import myimg from "../../public/next.svg"
 
 export default function Home() {
+  console.log(profile)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Next routing</h1>
-      <Link href="/productlist">product list</Link>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
+    
+     <h1>check this</h1>
+      <image 
+      src={profile.src}
+      width={500}
+      height={500}
+       />
+      
+
+      <image src='https://plus.unsplash.com/premium_photo-1707413391465-82ac03dd5555?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8' />
+      
     </main>
   );
 }
